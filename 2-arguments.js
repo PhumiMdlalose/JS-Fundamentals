afc6a6522d52591b67dbaa31f3be9c4d675de418
Phumelele <princessdlamini69@gmail.com>
2025-09-14 21:29:40 +0200

    Conditional statements in action

---
+++ 2-arguments.js
@@ -0,0 +1,13 @@
+const myArguments = (...myArgs) => {
+    let numArgs = myArgs.length;
+    if (myArgs.length === 0) {
+        console.log('No arguments found');
+    }
+    else if (myArgs.length === 1) {
+        console.log('Argument found');
+    }
+    else {
+        console.log('Arguments found');
+    }
+};
+myArguments();
