6648ec6f9d3a9a143fc7ee229183803f8186926e
Phumelele <princessdlamini69@gmail.com>
2025-09-14 23:28:34 +0200

    commit two strings

--- 4-concat.js
+++ 4-concat.js
@@ -1,6 +1,6 @@
 function argConcat (arg_1, arg_2){
     console.log(arg_1 + ' is ' + arg_2);
 }
-argConcat('Python', 'fun'); 
-argConcat('HBTN');
+//argConcat('Python', 'fun'); 
+//argConcat('HBTN');
 argConcat();
