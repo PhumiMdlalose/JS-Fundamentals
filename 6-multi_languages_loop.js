1908fa7f0a7734e7401642aef1c4d4d7c4964c99
Phumelele <princessdlamini69@gmail.com>
2025-09-20 23:48:50 +0200

    Loops and Arrays

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+const cLang = 'C is fun';
+const pythonLang = 'Python is cool';
+const jsLang = 'JavaScript is amazing';
+
+const languages = [cLang, pythonLang, jsLang];
+
+for (i = 0; i < languages.length; i++) {
+    console.log(languages[i]);
+}
